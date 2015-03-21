b46f3d2eeb7020781eb6017a388a9d6bc47b4bd4
flosama <orix.ambi@gmail.com>
2015-03-21 01:47:40 +0100

    datanode corrected

--- data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/markov/DataNode.java
+++ data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/markov/DataNode.java
@@ -3,9 +3,38 @@ package com.sixgroup.dfi.hackathon.dataenlightenment.markov;
 import com.sixgroup.dfi.hackathon.dataenlightenment.DataField;
 
 public class DataNode {
-    public DataField field;
-    public int count;
+    
+    private DataField field;
+    private int count;
 
-    public DataNode() {
+    public DataNode(DataField field, int count) {
+        if (field == null)
+            throw new IllegalArgumentException("No null value allowed");
+        if (count < 0)
+            throw new IllegalArgumentException("No count values below zero allowed");
+        this.field = field;
+        this.count = count;
     }
+
+    public DataField getField() {
+        return field;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || !Object.class.equals(DataNode.class) || !((DataNode) other).getField().equals(field)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return ("nodeof" + field.toString()).hashCode();
+    }
+
 }
--- data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/markov/MarkovChain.java
+++ data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/markov/MarkovChain.java
@@ -101,41 +101,4 @@ public class MarkovChain {
 
     // --- Inner types ---------------------------------------------------------
 
-    public class DataNode {
-
-        private DataField field;
-        private int count;
-
-        public DataNode(DataField field, int count) {
-            if (field == null)
-                throw new IllegalArgumentException("No null value allowed");
-            if (count < 0)
-                throw new IllegalArgumentException("No count values below zero allowed");
-            this.field = field;
-            this.count = count;
-        }
-
-        public DataField getField() {
-            return field;
-        }
-
-        public int getCount() {
-            return count;
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            if (other == null || !Object.class.equals(DataNode.class) || !((DataNode) other).getField().equals(field)) {
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            return ("nodeof" + field.toString()).hashCode();
-        }
-
-    }
-
 }
