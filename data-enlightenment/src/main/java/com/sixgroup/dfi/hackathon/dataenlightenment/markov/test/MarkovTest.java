ac340ca2ff615641d3928f129bd7fdd0059fc72b
flosama <orix.ambi@gmail.com>
2015-03-21 05:16:45 +0100

    nomal eine

--- data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/markov/test/MarkovTest.java
+++ data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/markov/test/MarkovTest.java
@@ -31,6 +31,7 @@ public class MarkovTest {
 
         DataField suffix1 = new DataField("56", "suffix1");
         DataField suffix2 = new DataField("78", "suffix2");
+        DataField suffix3 = new DataField("90", "suffix3");
 
         for (int i = 0; i < 700; ++i) {
             chain.increment(prefix, suffix1);
@@ -40,21 +41,32 @@ public class MarkovTest {
             chain.increment(prefix, suffix2);
         }
 
+        for (int i = 0; i < 1000; ++i) {
+            chain.increment(prefix, suffix3);
+        }
+
         int count1 = 0;
         int count2 = 0;
+        int count3 = 0;
 
         for (int i = 0; i < 1000; ++i) {
             DataField suffix = chain.getNextField(prefix);
-            if (suffix != null && suffix.equals(suffix1)) {
-                ++count1;
-            }
-            else {
-                ++count2;
+            if (suffix != null) {
+                if (suffix.equals(suffix1)) {
+                    ++count1;
+                }
+                else if (suffix.equals(suffix2)) {
+                    ++count2;
+                }
+                else {
+                    ++count3;
+                }
             }
         }
-        
-        System.out.println("Suffix 1 chosen (should be near 700): " + count1);
-        System.out.println("Suffix 2 chosen (should be near 300): " + count2);
+
+        System.out.println("Suffix 1 chosen (should be near 350): " + count1);
+        System.out.println("Suffix 2 chosen (should be near 150): " + count2);
+        System.out.println("Suffix 2 chosen (should be near 500): " + count3);
     }
 
 }
