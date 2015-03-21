91a6cc676102c7cfb43bb2f81c678561db9a3294
flosama <orix.ambi@gmail.com>
2015-03-21 01:39:28 +0100

    markov

--- data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/MarkovChain.java
+++ data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/MarkovChain.java
@@ -7,23 +7,24 @@
  */
 package com.sixgroup.dfi.hackathon.dataenlightenment;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author fmahler
- *
  */
 public class MarkovChain {
 
     // --- Fields --------------------------------------------------------------
-    
-    private Map<Integer, Node> nodes;
+
+    private Hashtable<List<DataField>, Set<DataNode>> continuations;
 
     // --- Constructors --------------------------------------------------------
-    
+
     public MarkovChain() {
-        nodes = new HashMap<>();
+        continuations = new Hashtable<>();
     }
 
     // --- Properties ----------------------------------------------------------
@@ -31,6 +32,28 @@ public class MarkovChain {
     // --- Creation ------------------------------------------------------------
 
     // --- Addition ------------------------------------------------------------
+
+    public void addContinuation(List<DataField> prefix, DataField suffix, int count) {
+        Set<DataNode> suffices = continuations.get(prefix);
+        if (suffices != null) {
+            suffices.add(new DataNode(suffix, count));
+        }
+        else {
+            suffices = new HashSet<>();
+            suffices.add(new DataNode(suffix, count));
+            continuations.put(prefix, suffices);
+        }
+    }
+
+    public void addMultipleContinuations(List<DataField> prefix, Set<DataNode> suffices) {
+        Set<DataNode> existingSuffices = continuations.get(prefix);
+        if (suffices != null) {
+            existingSuffices.addAll(suffices);
+        }
+        else {
+            continuations.put(prefix, suffices);
+        }
+    }
 
     // --- Access --------------------------------------------------------------
 
@@ -75,19 +98,42 @@ public class MarkovChain {
     // --- Private implementation ----------------------------------------------
 
     // --- Inner types ---------------------------------------------------------
-    
-    protected class Node {
-        
-        private DataField field;
 
-        public Node(DataField field){
+    public class DataNode {
+
+        private DataField field;
+        private int count;
+
+        public DataNode(DataField field, int count) {
+            if (field == null)
+                throw new IllegalArgumentException("No null value allowed");
+            if (count < 0)
+                throw new IllegalArgumentException("No count values below zero allowed");
             this.field = field;
+            this.count = count;
         }
-        
+
+        public DataField getField() {
+            return field;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
         @Override
-        public int hashCode(){
-            return field.hashCode();
+        public boolean equals(Object other) {
+            if (other == null || !Object.class.equals(DataNode.class) || !((DataNode) other).getField().equals(field)) {
+                return false;
+            }
+            return true;
         }
+
+        @Override
+        public int hashCode() {
+            return ("nodeof" + field.toString()).hashCode();
+        }
+
     }
 
 }
