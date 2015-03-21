7438aa075aecfa868bef3c45d9afb18209c54413
flosama <orix.ambi@gmail.com>
2015-03-21 01:45:07 +0100

    Node extracted

---
+++ data-enlightenment/src/main/java/com/sixgroup/dfi/hackathon/dataenlightenment/markov/DataNode.java
@@ -0,0 +1,11 @@
+package com.sixgroup.dfi.hackathon.dataenlightenment.markov;
+
+import com.sixgroup.dfi.hackathon.dataenlightenment.DataField;
+
+public class DataNode {
+    public DataField field;
+    public int count;
+
+    public DataNode() {
+    }
+}
